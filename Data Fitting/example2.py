fcc4dd013e7fc50c56db0e8619b88eb82c9a962f
Cesare De Cal <hi@cesare.io>
2019-10-08 22:12:07 +0200

    Printing coefficients

--- Data Fitting/example2.py
+++ Data Fitting/example2.py
@@ -15,25 +15,29 @@ import pylab as py
 import matplotlib.pyplot as plt
 from scipy.interpolate import CubicSpline
 from scipy.interpolate import lagrange
+from numpy.polynomial.polynomial import Polynomial
 
 # Points provided by the chemistry experiment
 # We store the x and y values in numpy arrays
 x = np.array([-1, -0.96, -0.86, -0.79, 0.22, 0.50, 0.93])
 y = np.array([-1.000, -0.151, 0.894, 0.986, 0.895, 0.500, -0.306])
-xnew = np.linspace(-1, 1, 1000) # For plotting
 
-# Draw the points
+xnew = np.linspace(-1, 1, 1000)
+
+# Let's first draw the points
 py.plot(x, y, 'bo', label='Point')
 
-# Compute interpolating polynomial with Lagrange method
+# Compute Interpolating Polynomial with Lagrange method
 poly = lagrange(x, y)
 plt.plot(xnew, poly(xnew), 'r', label='Interpolating Polynomial')
+print('Coefficients of interpolating polynomial:', Polynomial(poly).coef)
 
-# Compute natural cubic spline
-# ‘natural’: The second derivative at curve ends are zero
-s = CubicSpline(x, y, bc_type='natural')
+# Compute Natural Cubic Spline
+ncspline = CubicSpline(x, y, bc_type='natural')
+print('Coefficients of cubic spline:', ncspline.c)
 
-plt.plot(xnew, s(xnew), 'k-', label='Natural Cubic Spline', lw=1)
+plt.plot(xnew, ncspline(xnew), 'k-', label='Natural Cubic Spline', lw=1) # what's lw=1?
 
+# Final tweaks to plot
 plt.legend()
 plt.show()
