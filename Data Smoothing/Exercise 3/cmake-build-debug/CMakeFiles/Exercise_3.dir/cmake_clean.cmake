43dccd331578fccb9859988882fd2ac86ed54203
Cesare De Cal <hi@cesare.io>
2019-11-04 10:43:54 +0100

    Added C++ project

---
+++ Data Smoothing/Exercise 3/cmake-build-debug/CMakeFiles/Exercise_3.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Exercise_3.dir/main.cpp.o"
+  "Exercise_3"
+  "Exercise_3.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Exercise_3.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
