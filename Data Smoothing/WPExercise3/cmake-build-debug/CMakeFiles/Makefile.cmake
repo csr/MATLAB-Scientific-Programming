834a9af78156917137d4cd0164eefabfa5a6d0fb
Cesare De Cal <hi@cesare.io>
2019-11-05 16:19:50 +0100

    Added exercise data

--- Data Smoothing/WPExercise3/cmake-build-debug/CMakeFiles/Makefile.cmake
+++ Data Smoothing/WPExercise3/cmake-build-debug/CMakeFiles/Makefile.cmake
@@ -51,5 +51,5 @@ set(CMAKE_MAKEFILE_PRODUCTS
 
 # Dependency information for all targets:
 set(CMAKE_DEPEND_INFO_FILES
-  "CMakeFiles/exercise7.dir/DependInfo.cmake"
+  "CMakeFiles/WPExercise3.dir/DependInfo.cmake"
   )
---
+++ Data Smoothing/WPExercise3/cmake-build-debug/CMakeFiles/WPExercise3.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/WPExercise3.dir/main.c.o"
+  "WPExercise3"
+  "WPExercise3.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/WPExercise3.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
