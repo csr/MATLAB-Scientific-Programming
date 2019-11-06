f07070dd4face9bf754b1436d09c048e838c7588
Cesare De Cal <hi@cesare.io>
2019-11-06 19:08:12 +0100

    Modified plotting script

--- Data Smoothing/plotting.py
+++ Data Smoothing/plotting.py
@@ -5,6 +5,8 @@
 # Python program that can be executed to report whether particular
 # python packages are available on the system.
 
+import numpy as np
+import scipy
 import matplotlib.pyplot as plt
 
 x = [0.0, 0.6, 1.5, 1.7, 1.9, 2.1, 2.3, 2.6, 2.8, 3.0, 3.6, 4.7, 5.2, 5.7, 5.8, 6.0, 6.4, 6.9, 7.6, 8.0]
@@ -13,5 +15,26 @@ y = [-0.80, -0.34, 0.59, 0.59, 0.23, 0.10, 0.28, 1.03, 1.50, 1.44, 0.74, -0.82, 
 # Increase plot size
 plt.figure(dpi=130)
 
+# coefficients
+lambdas = [-0.530105183842687, 0.837166463073442, -2.720645879111871, -0.785685549556168]
+
+def basisFunction(x, i):
+    return scipy.special.binom(3, i)*pow(x, i)*pow(1-x, 3-i)
+
+def myFunction(x):
+    firstTerm = lambdas[0]*basisFunction(x, 0)
+    secondTerm = lambdas[1]*basisFunction(x, 1)
+    thirdTerm = lambdas[2]*basisFunction(x, 2)
+    fourthTerm = lambdas[2]*basisFunction(x, 3)
+    return firstTerm + secondTerm + thirdTerm + fourthTerm
+
+linSpace = np.linspace(min(x), max(x), 1000)
+
+functionYValues = []
+
+for i, val in enumerate(linSpace): 
+    functionYValues.append(myFunction(val))
+
 plt.scatter(x, y)
+plt.plot(linSpace, functionYValues)
 plt.show()
