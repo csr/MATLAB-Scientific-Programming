890ea43f92b0cfa8335baac31ed8a38b11cefa21
Cesare De Cal <hi@cesare.io>
2019-11-25 19:21:21 +0100

    Plotting

--- Orthogonal Basis Function/exercise3.m
+++ Orthogonal Basis Function/exercise3.m
@@ -4,62 +4,85 @@
 % author: Cesare De Cal
 % ----------------------------------------------
 
-% Function given by exercise. Multiplier can be 2 or 4
-func = @(multiplier, x) cos(multiplier*x) 
+cosMultiplier = 2;
+
+% Function given by exercise. Tweak cosMultiplier above
+func = @(multiplier, x) cos(multiplier*x);
 
 % Chebyshev compact-form formula
-chebyshevFunction = @(index, x) cos(index*acos(x))
-chebyshevRootFunction = @(index, nrOfPoints) cos(((2*index-1)*pi)/(2*nrOfPoints))
+chebyshevFunction = @(i, x) cos(i*acos(x));
 
-degree = 6
-nrOfPoints = degree + 1
+degree = 6;
+nrOfPoints = degree + 1;
 
 % Calculate roots of Chebyshev polynomial
 roots = 1:nrOfPoints;
 for index = 1:nrOfPoints
-    roots(index) = chebyshevRootFunction(index-1, nrOfPoints)
+    cosNumerator = (2*index-1)*pi;
+    cosDenominator = 2*nrOfPoints;
+    roots(index) = cos(cosNumerator/cosDenominator);
+end
+
+% Calculate coefficients of Chebyshev polynomial
+% Rescale interval from [-pi, pi] to [-1, 1] by
+% multiplying argument by pi
+coefficients = 1:nrOfPoints;
+for j = 1:nrOfPoints
+    % Sum
+    sum = 0;
+    for k = 1:nrOfPoints
+        root = roots(k);
+        functionEvaluation = func(cosMultiplier, pi*root);
+        chebyRes = chebyshevFunction(j-1, root);
+        sum = sum + functionEvaluation*chebyRes;
+    end
+    coefficients(j) = (2/nrOfPoints)*sum;
+end
+
+% Calculate associated y values for roots
+rootsY = 1 = cos(cosMultiplier*pi*data_x);
+
+% Compute Y points
+nrGraphXPoints = 1000;
+computedYValues = 1:nrGraphXPoints;
+x = linspace(-1,1,nrGraphXPoints);
+for r = 1:nrGraphXPoints
+    point = x(r);
+    sum = 0;
+	for j = 1:nrOfPoints
+        coefficient = coefficients(j);
+        chebyRes = chebyshevFunction(j-1, rootX);
+		sum = sum + coefficient*chebyRes;
+    end
+	computedYValues(r) = sum - firstCoeff;
 end
 
 'Chebyshev polynomial roots:'
 roots'
 
-% Calculate coefficients of Chebyshev polynomial
-% Rescale interval from [-pi, pi] to [-1, 1] by
-% multiplying argument by pi
-coefficients = 1:nrOfPoints
-for j = 1:nrOfPoints
-    % Sum
-    sum = 0
-    for k = 1:nrOfPoints
-        root = roots(k)
-        functionEvaluation = func(2, pi*root)
-        chebyRes = chebyshevFunction(j-1, root)
-        sum = sum + functionEvaluation*chebyRes
-    end
-    coefficients(j) = (2/nrOfPoints)*sum
-end
-
 'Chebyshev polynomial coefficients:'
 coefficients'
-
-% Calculate associated y values for roots
-rootsY = 1:nrOfPoints;
-firstCoeff = coefficients(1)/2;
-for r = 1:nrOfPoints
-    rootX = roots(r)
-    sum = 0;
-	for j = 1:nrOfPoints
-        coefficient = coefficients(j)
-        chebyRes = chebyshevFunction(j-1, rootX)
-		sum = sum + coefficient*chebyRes;
-    end
-	rootsY(r) = sum - firstCoeff;
-end
 
 'Y values of root:'
 rootsY'
 
-% Create array of equi-distant points to plot
-% linspace = linspace(-1,1,1000)
-
 % Plotting
+
+
+% Roots
+plot(roots, rootsY, 'o', 'LineWidth', 2)
+hold on
+
+% Actual function
+plot(x, cos(cosMultiplier*pi*roots), 'LineWidth', 2)
+hold on
+
+% Chebyshev Polynomial Approximation
+plot(x, computedYValues, 'LineWidth', 2)
+hold off
+
+axis([-1 1 -1.2 1.2])
+grid on
+
+ylabel('Y')
+xlabel('X')
