f634012af5d164356eb58570c79fed17e7103422
Cesare De Cal <hi@cesare.io>
2019-11-29 13:37:56 +0100

    Minor fixes

--- Random Number Generation/Example/compile.sh
+++ Random Number Generation/Example/compile.sh
@@ -1,4 +1,4 @@
 source /opt/intel/compilers_and_libraries_2019.5.281/mac/mkl/bin/mklvars.sh intel64
-g++-9 -fopenmp -m64 -I${MKLROOT}/include -c template.c
-g++-9 -L${MKLROOT}/lib -lmkl_intel_lp64 -lmkl_gnu_thread -lmkl_core -lgomp -lpthread -lm -ldl template.o -o template
+gcc-9 -fopenmp -m64 -I${MKLROOT}/include -c template.c
+gcc-9 -L${MKLROOT}/lib -Wl,--no-as-needed -lmkl_intel_lp64 -lmkl_gnu_thread -lmkl_core -lgomp -lpthread -lm -ldl template.o -o template
 ./template
